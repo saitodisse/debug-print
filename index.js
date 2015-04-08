09e7a214ac1b179e642a5d07af0e47d0138f814f
Julio Makdisse Saito <saitodisse@gmail.com>
2015-04-08 00:00:39 -0300

    depth null

--- index.js
+++ index.js
@@ -5,15 +5,12 @@ module.exports = function(debug, obj) {
     return;
   }
 
-  var depth = process.env.DEPTH;
-  if (!depth) {
-    depth = 1;
-  }
+  var depth_env = parseInt(process.env.DEPTH) || null;
 
   var arguments_string = '';
   for (var i = 0; i < obj.arguments.length; i++) {
     arguments_string += util.inspect(obj.arguments[i],
-      { showHidden:false, colors:true, depth: depth });
+      { showHidden:false, colors:true, depth: depth_env });
     if (i !== obj.arguments.length - 1) {
       arguments_string += ', ';
     }
@@ -23,7 +20,7 @@ module.exports = function(debug, obj) {
     '',
     '    ' + obj.line.original_line + ' | ' + obj.name + ' (' + arguments_string + ')',
     '      |   returned: ' + util.inspect(obj.return_data,
-      { showHidden:false, colors:true, depth: depth }),
+      { showHidden:false, colors:true, depth: depth_env }),
     '      |   time elapsed:',
   ].join('\n');
 
